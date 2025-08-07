dacc1d9485306011643349d9fd9fda7c0085f48f
Alek-Grujic <alek.grujic001@outlook.com>
2025-08-07 06:38:46 +0200

    Refactor modal logic into reusable open and close functions

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
-btn.addEventListener('click', function () {
+function closeModal() {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btn.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
